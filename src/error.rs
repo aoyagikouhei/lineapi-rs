18e5fe0fc03b883178603f072a2871bcef9aa99d
aoyagikouhei <aoyagi.kouhei@gmail.com>
2026-06-05 18:31:54 +0900

    modify error

--- src/error.rs
+++ src/error.rs
@@ -8,7 +8,10 @@ use crate::LineResponseHeader;
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct LineLoginErrorResponse {
     pub error: String,
-    pub error_description: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub error_description: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub error_uri: Option<String>,
     #[serde(flatten)]
     pub extra: std::collections::HashMap<String, serde_json::Value>,
 }
